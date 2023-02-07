2a724311c6cba549b4d2b603bbeefdc56af635e0
Nanachiser <Nanachiser@users.noreply.github.com>
2023-02-07 14:26:55 +0900

    README.md을(를) 추가합니다.

--- js/share.js
+++ js/share.js
@@ -3,11 +3,16 @@ function kakaoShare() {
 
     Kakao.Share.sendDefault({
         objectType: 'text',
+        content: {
+            title: shareTitle, 
+            description: shareDes,
+            imageUrl: shareImage; 
+            }
         text:
             '기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다.',
         link: {
-            mobileWebUrl: 'https://5lovinglanguages-kr.netlify.app',
-            webUrl: 'https://5lovinglanguages-kr.netlify.app',
+            mobileWebUrl: 'shareURL',
+            webUrl: 'shareURL',
         },
     });
 
