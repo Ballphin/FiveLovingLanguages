c726aad1e225bf1db5fea1d7dc99cf32458d1368
Nanachiser <Nanachiser@users.noreply.github.com>
2023-02-07 15:56:19 +0900

    에러수정

--- js/share.js
+++ js/share.js
@@ -1,10 +1,12 @@
-const url = 'https://5lovinglanguages-kr.netlify.app/'; 
+
+const url = 'https://5lovinglanguages-kr.netlify.app/';
+const name = '5가지 사랑의언어';
 
 function setShare() {
     var resultImg = document.querySelector('#resultImg');
     var resultAlt = resultImg.firstElementChild.alt;
 
-    const shareTitle = "이이잉버그";
+    const shareTitle = name;
     const shareDes = infoList[resultAlt].name;
     const shareImage = url + 'img/image-' + resultAlt + '.png';
     const shareURL = url + 'page/result-' + resultAlt + '.html';
@@ -23,7 +25,7 @@ function setShare() {
 
         buttons: [
             {
-                title: '1234',
+                title: 'Share',
                 link: {
                     mobileWebUrl: shareURL,
                     webUrl: shareURL,
